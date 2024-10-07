d9487b448db1c82cb25ed71f18290ce191042bfa
vaibhav <160115194+vaibhavsalave@users.noreply.github.com>
2024-10-07 07:59:43 +0000

    datatypesummaray

---
+++ Basics/datatypessummary.js
@@ -0,0 +1,50 @@
+/* 
+    Primitive    
+   
+    Call  By Value -  using copy 
+     7 Types : 
+          1.String 
+          2.Number
+          3.Boolean 
+          4.null
+          5.undefined 
+          6.Symbol
+          7.BigInt 
+
+   */
+
+  //javascript is a dyanmically type 
+    /*
+
+       Referance(Non primitive)
+       
+            Array 
+            Object 
+            Functions
+    
+    */
+   const heros = ["shaktiman", "naagraj", "doga"] ;
+ let obj =  {
+       name :"vaibhav",
+       aaa: 22
+   }
+   const myfunction = function(){   // datatype using typeof is "object function" 
+      console.log("hello viabhav");
+      
+   }
+//    null cha data type tumhala object 
+
+/* 
+        typeof operator result 
+
+    type of val                                    result
+
+    Undefined                                    undefined
+    Null                                         object 
+    Boolean                                      boolean
+    Number                                       number 
+    String                                       string 
+
+
+
+*/
