90e49e8bfd7a797845e2855e988d8f2997ce9094
vaibhav <160115194+vaibhavsalave@users.noreply.github.com>
2024-10-07 04:09:43 +0000

    conversion of datatypes

---
+++ Basics/03_conversionOperation.js
@@ -0,0 +1,53 @@
+
+let score =  33  ;
+
+
+console.log(typeof(score)) ; //number
+
+let str =  "44" ;
+console.log(typeof str);
+let valInNumber = Number(str)
+console.log(typeof valInNumber);
+
+//  type of NAN  
+
+let scre = "33";
+
+let valNumber = Number(scre) //number 
+// console.log(valNumber);
+
+
+
+//  when print typeof  valNumber in NaN 
+
+// console.log(typeof valNumber );  // 
+let c =  valNumber + score ;
+console.log(c);
+    
+/*  
+   1) if store the value   in varaible is null   
+        
+        then typeof the variable is   object 
+
+        when conert this varaible in  in number and store in another varaible 
+        
+         then value is  give a Zero
+
+   2) value is undefiend  
+         then typeof undefined 
+
+         conversion in number then value NaN
+  3) boolean 
+       typeof  boolean 
+
+       convert Number  true 1 and false 0  
+*/
+
+// if we do string is empty  and convert it boolean then give output as false
+
+// if string avlai  then give a true 
+
+let num =  33 ;
+
+let stringNumber =  String(num) // type of num is string and store in stringNumber 
+
