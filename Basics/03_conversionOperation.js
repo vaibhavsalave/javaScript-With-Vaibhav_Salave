7a46dd4447969428bb094ac56522fc1044caffd9
vaibhav <160115194+vaibhavsalave@users.noreply.github.com>
2024-10-07 04:32:23 +0000

    conversion of datatypes

--- Basics/03_conversionOperation.js
+++ Basics/03_conversionOperation.js
@@ -51,3 +51,48 @@ let num =  33 ;
 
 let stringNumber =  String(num) // type of num is string and store in stringNumber 
 
+//***********************************************************Operations */
+
+    let value =  3 ;
+    let negvalue = -value 
+    console.log(negvalue)  //  - 3 
+
+    // string 1st assign value  "hello" and  string 2nd assign value  "vaibhav"
+
+    let str1 = "hello" 
+    let str2 = "vaibhav" 
+    let str3 =  str1 + str2 ;
+
+    console.log(str3); // hello vaibhav
+
+    console.log("1" + 2);  // 12
+    console.log(1+ "2");  //12
+    console.log("1" +2 +2); //122
+    console.log(1+2+ "2"); //32
+
+//     triciy conversion 
+
+  console.log(true); // true 
+
+  console.log( +true); // 1
+
+  //console.log(true+); // error
+
+  console.log(+""); // 0
+ 
+    let num1 ,num2 , num3  ;
+    num1 = num2 =num3 = 2+2 ; // 4 
+
+
+    let gameCounter = 100 
+    gameCounter++ ;   // ++gameCounter 
+    console.log(gameCounter); //101
+    
+  
+  
+  
+    
+    
+    
+    
+    
