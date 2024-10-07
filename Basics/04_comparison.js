7b85df1e949e54d00eeee68522ad88c61c5ad39b
vaibhav <160115194+vaibhavsalave@users.noreply.github.com>
2024-10-07 04:46:16 +0000

    comparison

---
+++ Basics/04_comparison.js
@@ -0,0 +1,17 @@
+
+//predicatable result not occured 
+console.log("2" > 1); //true
+
+console.log("02" > 1);  //true  
+
+//comparison with null 
+// in comaprison time null convert into number , treating it as 0  
+console.log(null > 0 ); //false
+console.log(null == 0 ); //false
+// value conversion 
+console.log(null >= 0);  //true
+
+// === check datatype also 
+
+
+
